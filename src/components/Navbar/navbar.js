068bba7fa288d336280ca09317dfa1191a86f668
Sparky <17860180+sparkins@users.noreply.github.com>
2019-03-04 15:27:13 -0800

    added a placeholder navbar

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container, Image, Menu, Button, Header } from 'semantic-ui-react';
+import './navbar.css';
+
+// Create a sticky navbar that show the title of the game, a dynamic message and the scoreboard
+class Navbar extends React.Component {
+
+    render() {
+        return (
+                    <Container className="navbar">
+                        <Header className="header" as='a' active>Portfolio Generator</Header>
+                        <Button className="projectPage" as='a'>Projects Page</Button>
+                        <Button className="profilePage" as='a'>Profile Page</Button>
+                    </Container>
+        );
+    }
+}
+
+export default Navbar;
