9b966812bb42225eaa1e49bd934e128601569fb6
Sparky <17860180+sparkins@users.noreply.github.com>
2019-03-04 15:09:04 -0800

    created server.js, added a number of modules via yarn add

---
+++ server.js
@@ -0,0 +1,47 @@
+// Dependencies
+const express = require("express");
+const path = require("path");
+var mongojs = require("mongojs");
+var logger = require("morgan");
+var bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Set the app up with morgan
+app.use(logger("dev"));
+
+// set the app up with bodyparser
+app.use(bodyParser());
+
+// Database configuration
+var databaseUrl = process.env.MONGODB_URI || "portfoliogenerator_db";
+var collections = ["projects", "users"];
+
+// Hook mongojs config to db variable
+var db = mongojs(databaseUrl , collections);
+
+// Log any mongojs errors to console
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+
+// Define API routes here
+
+// Send every other request to the React app
+// Define any API routes before this runs
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
