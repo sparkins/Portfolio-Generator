edca476267078e51a03b80094232f2b9bb3bb656
mingmingc <caressi.m@gmail.com>
2019-03-13 20:37:36 -0700

    connecting user input to db, getting error Unhandled Rejection (SyntaxError): Unexpected token < in JSON at position 0

--- models/Users.js
+++ models/Users.js
@@ -2,12 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    name: { type: String, required: true }, //full name
     username: { type: String, required: true }, //the ID name they use to log in
-    passwordHash: { type: String, required: true }, 
+    passwordHash: { type: String, required: true },
+    firstName: { type: String, required: true }, 
+    lastName: { type: String, required: true }, 
+    portfolioName: { type: String, required: true },
     bio: { type: String, required: true },
-    layout: { type: String, required: true },
-    textColor: { type: String, required: true }
+    experience: { type: String, required: true },
+    skills: { type: String, required: true },
+    projectOutlines: { type: String, required: true }
 });
 
 const User = mongoose.model("User", userSchema);
